0902c4e1df2cebe8ae6f5e7b3896d11fba7acb40
Brady Loomis <kloomis2@wisc.edu>
2014-04-07 11:45:34 -0500

    good through 9.2.2

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -36,7 +36,6 @@ describe "Authentication" do
       it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-      it { should have_link('Users',       href: users_path) }
       
       describe "followed by signout" do
         before { click_link "Sign out" }
