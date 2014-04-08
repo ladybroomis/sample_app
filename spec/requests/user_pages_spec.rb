19aea0573f4ac2b0507d726c5b275198fde29d66
Brady Loomis <kloomis2@wisc.edu>
2014-04-08 16:01:48 -0500

    end chapter 9 before exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,26 +27,26 @@ describe "User pages" do
         end
       end
     end
-  end
+  
+    describe "delete links" do
 
-  describe "delete links" do
+      it { should_not have_link('delete') }
 
-    it { should_not have_link('delete') }
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
 
-    describe "as an admin user" do
-      let(:admin) { FactoryGirl.create(:admin) }
-      before do
-        sign_in admin
-        visit users_path
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
       end
-
-      it { should have_link('delete', href: user_path(User.first)) }
-      it "should be able to delete another user" do
-        expect do
-          click_link('delete', match: :first)
-        end.to change(User, :count).by(-1)
-      end
-      it { should_not have_link('delete', href: user_path(admin)) }
     end
   end
 
@@ -103,40 +103,32 @@ describe "User pages" do
 
     let(:submit) { "Create my account" }
 
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end 
+
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-    
-
-      describe "after submission" do
-        before { click_button submit }
-
-        it { should have_title('Sign up') }
-        it { should have_content('error') }
-      end
-    end
-  end
-     
-  describe "with valid information" do
-    before do
-      fill_in "Name",         with: "Example User"
-      fill_in "Email",        with: "user@example.com"
-      fill_in "Password",     with: "foobar"
-      fill_in "Confirmation", with: "foobar"
-    end
-
-    describe "after saving the user" do
-      before { click_button submit }
-      let(:user) { User.find_by(email: 'user@example.com') }
-
-      it { should have_link('Sign out') }
-      it { should have_title(user.name) }
-      it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-    end
-
-    it "should create a user" do
-      expect { click_button submit }.to change(User, :count).by(1)
     end
   end
 end
