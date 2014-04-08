fd80c1878be06c94b0a8a9091705701767811c6c
Brady Loomis <kloomis2@wisc.edu>
2014-04-08 18:31:29 -0500

    exercise 8

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -65,7 +65,7 @@ describe "Authentication" do
         it {should_not have_link('Settings')}
         it {should_not have_link('Sign out')}
       end
-      
+
       describe "in the Users controller" do
             
         describe "visiting the user index" do
@@ -87,8 +87,38 @@ describe "Authentication" do
             expect(page).to have_title('Edit user')
           end
         end
+      end 
+        
+    describe "when attempting to visit a protected page" do
+      before do
+        visit edit_user_path(user)
+        fill_in "Email",    with: user.email
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      describe "after signing in" do
+
+        it "should render the desired protected page" do
+          expect(page).to have_title('Edit user')
+        end
+
+        describe "when signing in again" do
+          before do
+            click_link "Sign out"
+            visit signin_path
+            fill_in "Email",    with: user.email
+            fill_in "Password", with: user.password
+            click_button "Sign in"
+          end
+
+          it "should render the default (profile) page" do
+            expect(page).to have_title(user.name)
+          end
+        end
       end
     end
+  end
 
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
