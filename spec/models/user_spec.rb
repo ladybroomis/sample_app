e54d3cfd6bdbeb358be3e33de6e7d026964816de
Brady Loomis <kloomis2@wisc.edu>
2014-04-17 15:49:09 -0500

    10.1.4

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,7 +19,8 @@ describe User do
   	it { should respond_to(:admin) }
   	it { should respond_to(:microposts) }
 
-	it { should be_valid }
+  	it { should be_valid }
+  	it { should_not be_admin }
 
 	describe "when name is not present" do
 		before { @user.name = " " }
@@ -68,20 +69,13 @@ describe User do
 	end
 
 	describe "email address with mixed case" do
-    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
-
-    it "should be saved as all lower-case" do
+    	let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    	it "should be saved as all lower-case" do
       @user.email = mixed_case_email
       @user.save
       expect(@user.reload.email).to eq mixed_case_email.downcase
-    end
-  end
-
-  	it { should respond_to(:authenticate) }
-  	it { should respond_to(:admin) }
-
-  	it { should be_valid }
-  	it { should_not be_admin }
+    	end
+    end 
 
   	describe "with admin attribute set to 'true'" do
     	before do
@@ -125,8 +119,33 @@ describe User do
 			specify { expect(user_for_invalid_password).to be_false }
 		end
 	end
+
 	describe "remember token" do
-    before { @user.save }
-    its(:remember_token) { should_not be_blank }
-  end
+    	before { @user.save }
+    	its(:remember_token) { should_not be_blank }
+    end 
+
+    describe "micropost associations" do
+
+  		before { @user.save }
+    	let!(:older_micropost) do
+      	  FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    	end
+    	let!(:newer_micropost) do
+      	  FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    	end
+
+    	it "should have the right microposts in the right order" do
+     	  expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+   		end
+
+   		it "should destroy associated microposts" do
+    	  microposts = @user.microposts.to_a
+    	  @user.destroy
+    	  expect(microposts).not_to be_empty
+    	  microposts.each do |micropost|
+    	    expect(Micropost.where(id: micropost.id)).to be_empty
+    	  end
+    	end
+  	end
 end
