1ef349eec0a83f0a752ca88d7a33864b08e2916e
Brady Loomis <kloomis2@wisc.edu>
2014-04-17 14:50:16 -0500

    10.1.2-10.1.3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User do
 
 	before do
 		@user = User.new(name: "Example User", email: "user@example.com",
-			password: "foobar", password_confirmation: "foobar")
+						password: "foobar", password_confirmation: "foobar")
 	end
 
 	subject { @user }
@@ -16,6 +16,8 @@ describe User do
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:authenticate) }
   	it { should respond_to(:remember_token) }
+  	it { should respond_to(:admin) }
+  	it { should respond_to(:microposts) }
 
 	it { should be_valid }
 
