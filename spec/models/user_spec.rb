ff217bdfee743493c75be60f5f89474c2d5ba0a8
Brady Loomis <kloomis2@wisc.edu>
2014-04-22 18:17:41 -0500

    before 11.3.3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -133,39 +133,50 @@ describe User do
     	its(:remember_token) { should_not be_blank }
     end 
 
-    describe "micropost associations" do
+  	describe "micropost associations" do
 
-  		before { @user.save }
-    	let!(:older_micropost) do
-      	  FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+ 	  before { @user.save }
+      let!(:older_micropost) do
+      	FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+      end
+      let!(:newer_micropost) do
+      	FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+      end
+
+      it "should have the right microposts in the right order" do
+     	expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+   	  end
+
+   	  it "should destroy associated microposts" do
+    	microposts = @user.microposts.to_a
+        @user.destroy
+    	expect(microposts).not_to be_empty
+    	microposts.each do |micropost|
+    	  expect(Micropost.where(id: micropost.id)).to be_empty
     	end
-    	let!(:newer_micropost) do
-      	  FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
-    	end
+      end
 
-    	it "should have the right microposts in the right order" do
-     	  expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
-   		end
-
-   		it "should destroy associated microposts" do
-    	  microposts = @user.microposts.to_a
-    	  @user.destroy
-    	  expect(microposts).not_to be_empty
-    	  microposts.each do |micropost|
-    	    expect(Micropost.where(id: micropost.id)).to be_empty
-    	  end
-    	end
-
-    	describe "status" do
-      	  let(:unfollowed_post) do
-         	FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
-      	  end
-
-      	  its(:feed) { should include(newer_micropost) }
-          its(:feed) { should include(older_micropost) }
-          its(:feed) { should_not include(unfollowed_post) }
+      describe "status" do
+        let(:unfollowed_post) do
+          FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
         end
-  	end
+        let(:followed_user) { FactoryGirl.create(:user) }
+
+        before do
+          @user.follow!(followed_user)
+          3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+        end
+
+        its(:feed) { should include(newer_micropost) }
+        its(:feed) { should include(older_micropost) }
+        its(:feed) { should_not include(unfollowed_post) }
+        its(:feed) do
+          followed_user.microposts.each do |micropost|
+            should include(micropost)
+          end
+        end
+      end
+    end 
 
   	describe "following" do
    	  let(:other_user) { FactoryGirl.create(:user) }
